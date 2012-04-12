02d13610e7110d8a021c65dc3627584842d05f04
George Pantazis <gcpantazis@gmail.com>
2012-04-11 23:25:24 -0600

    Event should be triggered inside callback

--- app/controllers/appController.js
+++ app/controllers/appController.js
@@ -48,8 +48,8 @@ exports.init = Backbone.Router.extend({
 		// Conditional switch for process.env.PORT flag from Heroku.
 		view.app.listen(process.env.PORT || 3000, function(){
 			console.log("Express  server listening on port %d in %s mode", view.app.address().port, view.app.settings.env);
+			global.trigger('ready:app', view.app);
 		});
 
-		global.trigger('ready:app', view.app);
 	}
 });
